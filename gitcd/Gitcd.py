b4c4635b870d0a640b1f95ef8fe9ad9a72bf4340
Claudio Walser <claudio.walser@srf.ch>
2016-03-08 18:32:42 +0100

    minimal working product

--- gitcd/Gitcd.py
+++ gitcd/Gitcd.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 from gitcd.Exceptions import GitcdException
+#from gitcd.ConfigFile import ConfigFile as Gitcdfile
+from gitcd.Interface.AbstractInterface import AbstractInterface
+from gitcd.Interface.Cli import Cli
 
 """
 Gitcd implementation.
 """
 class Gitcd(object):
+  """
+  interface: knack.Interface.AbstractInterface.AbstractInterface
+  """
+  interface = False
 
+  def __init__(self, interface: AbstractInterface):
+    self.interface = interface
 
   def init(self):
     print("initialize gitcd tool")
