f3cbe52e0d65e8d6647815b25c79a836db93fb41
Claudio Walser <claudio.walser@srf.ch>
2016-06-01 18:22:02 +0200

    Add some debug for debian box

--- gitcd/Cli/Command.py
+++ gitcd/Cli/Command.py
@@ -1,10 +1,14 @@
 import subprocess
 import string
 
+from pprint import pprint
+
 class Command(object):
 
   def execute(self, command: str):
     cliArgs = self.parseCliArgs(command)
+
+    pprint(cliArgs)
 
     process = subprocess.Popen(cliArgs, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, err = process.communicate()
