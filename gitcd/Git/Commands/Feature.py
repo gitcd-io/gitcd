23dd39b8167d67f30e30f12ad8c1ae0796ea5a82
Claudio Walser <claudio.walser@srf.ch>
2016-06-02 08:55:06 +0200

    Removed debug output and set proper comment

--- gitcd/Git/Commands/Feature.py
+++ gitcd/Git/Commands/Feature.py
@@ -73,10 +73,7 @@ class Feature(Command):
     # push new tag
     currentDate = time.strftime("%Y.%m.%d%H%M")
     tagMessage = self.interface.askFor("What message your new tag should have?")
-    # you are not allowed to have double quotes in the tag message because of parsing the arguments
-    # i dont want to write a lexical parser for this
-    tagMessage = tagMessage.replace('"', '\\"')
-    print(tagMessage)
+    # escape double quotes for shell command
     self.cli.execute('git tag -a -m "%s" %s%s' % (tagMessage, self.config.getTag(), currentDate))
     self.cli.execute("git push %s %s%s" % (origin, self.config.getTag(), currentDate))
 
