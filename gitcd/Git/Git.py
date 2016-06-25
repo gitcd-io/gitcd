803ef0d2b46554dda758d3bba61efe6032f49d1b
Claudio Walser <claudio@claudio-laptop>
2016-06-25 16:47:28 +0200

    Added release as new command

--- gitcd/Git/Git.py
+++ gitcd/Git/Git.py
@@ -1,6 +1,7 @@
 from gitcd.Git.Commands.Init import Init
 from gitcd.Git.Commands.Clean import Clean
 from gitcd.Git.Commands.Feature import Feature
+from gitcd.Git.Commands.Release import Release
 from gitcd.Git.Abstract import Abstract
 
 class Git(Abstract):
@@ -8,10 +9,12 @@ class Git(Abstract):
   commands = {
     'init': Init(),
     'clean': Clean(),
-    'feature': Feature()
+    'feature': Feature(),
+    'release': Release()
   }
 
   def setupCommands(self):
     self.commands['init'].setConfig(self.config)
     self.commands['clean'].setConfig(self.config)
     self.commands['feature'].setConfig(self.config)
+    self.commands['release'].setConfig(self.config)
