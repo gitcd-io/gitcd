caa7daad45d53a31f7de77eb908bacbf24375af2
Claudio Walser <claudio@claudio-laptop>
2016-06-25 16:10:18 +0200

    type hint for origin and new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='gitcd',
-      version='1.0.3',
+      version='1.0.4',
       description='Tool for continous delivery using git',
       author='Claudio Walser',
       author_email='claudio.walser@srf.ch',
