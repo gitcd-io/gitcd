82e8aeb64383e1b1f3c007c50f79af4bc6274007
Claudio Walser <claudio@claudio-laptop>
2016-06-25 16:56:29 +0200

    version pump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='gitcd',
-      version='1.0.4',
+      version='1.0.5',
       description='Tool for continous delivery using git',
       author='Claudio Walser',
       author_email='claudio.walser@srf.ch',
