92595871f908aa22d353a2490f851da23f3d1f64
Claudio Walser <claudio.walser@srf.ch>
2016-06-29 10:36:28 +0200

    Add .gitcd-personal to .gitignore automaticly

--- gitcd/Config/FilePersonal.py
+++ gitcd/Config/FilePersonal.py
@@ -24,6 +24,23 @@ class FilePersonal:
   def write(self):
     self.parser.write(self.filename, self.config)
 
+    # add .gitcd-personal to .gitignore
+    gitignore = ".gitignore"
+    if not os.path.isfile(gitignore):
+      gitignoreContent = self.filename
+    else:
+      with open(gitignore, "r") as gitignoreFile:
+        gitignoreContent = gitignoreFile.read()
+      # if not yet in gitignore
+      if "\n%s\n" % (self.filename) not in gitignoreContent:
+        # add it
+        gitignoreContent = "%s\n%s\n" % (gitignoreContent, self.filename)
+
+
+    with open(gitignore, "w") as gitignoreFile:
+      gitignoreFile.write(gitignoreContent)
+
+
   def getToken(self):
     return self.config['token']
 
