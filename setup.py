55fbcd83057e47ac436143eabc9c231ddcc97af8
Claudio Walser <claudio.walser@srf.ch>
2016-07-01 08:05:32 +0200

    new minor release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='gitcd',
-    version='1.0.5',
+    version='1.1.0',
     description='Tool for continous delivery using git',
     author='Claudio Walser',
     author_email='claudio.walser@srf.ch',
