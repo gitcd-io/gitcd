d0f0f7c4a5d418b5f4bbd56743e474210a607043
Claudio Walser <claudio.walser@srf.ch>
2016-09-19 08:57:05 +0200

    Implemented compare branch as argument

--- gitcd/Git/Commands/Compare.py
+++ gitcd/Git/Commands/Compare.py
@@ -3,9 +3,15 @@ from gitcd.Git.Command import Command
 
 class Compare(Command):
 
-    def execute(self, dummy: str):
+    def execute(self, branchToCompare: str):
         self.update()
         origin = self.getOrigin()
+
         currentBranch = self.getCurrentBranch()
-        latestTag = self.getLatestTag()
-        self.cli.execute("git diff %s %s --color" % (latestTag, currentBranch))
+        if not self.checkBranch(origin, currentBranch):
+            return False
+
+        if branchToCompare == "*":
+            branchToCompare = self.getLatestTag()
+
+        self.cli.execute("git diff %s %s --color" % (branchToCompare, currentBranch))
