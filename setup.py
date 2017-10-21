7f4390ea54d7cc8a8b8f3e527f09fa7538b43d42
Claudio Walser <claudio.walser@srf.ch>
2017-10-21 11:25:44 +0200

    setup.py license and classifiers

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='gitcd',
@@ -9,18 +9,23 @@ setup(
     author='Claudio Walser',
     author_email='claudio.walser@srf.ch',
     url='https://github.com/claudio-walser/gitcd',
-    packages=[
-        '.',
-        'gitcd',
-        'gitcd.Cli',
-        'gitcd.Config',
-        'gitcd.Git',
-        'gitcd.Git.Commands'
-    ],
+    packages=find_packages(),
     install_requires=['pyyaml', 'argparse', 'argcomplete', 'requests'],
     entry_points={
         'console_scripts': [
             'git-cd = gitcd.__main__:main'
         ]
-    }
+    },
+    license = 'Apache License',
+    keywords = ['git', 'application', 'continuos delivery'],
+    classifiers = [
+        'Development Status :: Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities'
+    ]
 )
