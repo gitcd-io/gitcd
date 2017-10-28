fbb6fb84c8765dfb10cad69e02ce7b224d327a2f
claudio-walser <walsercl@gmx.ch>
2017-10-28 09:11:12 +0200

    return of non-zero return code

--- .travis/test-git-cd.sh
+++ .travis/test-git-cd.sh
@@ -1,9 +1,10 @@
 # change workdir to travis-gitcd
 cd ~/build/claudio-walser/travis-gitcd
-git checkout master
-git pull origin master
 
 git-cd start travis-$TRAVIS_JOB_NUMBER
+if [ $? != 0 ] then
+    return 1
+fi
 echo $?
 # change back to original workdir
 cd -
