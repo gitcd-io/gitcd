0103b49e88c8d76f50449163c9d70f1217cd3058
claudio-walser <walsercl@gmx.ch>
2017-10-28 09:55:25 +0200

    setup.py no 3.2 let travis for now to test

--- setup.py
+++ setup.py
@@ -41,8 +41,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Utilities'
     ]
 )
