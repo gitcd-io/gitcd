0cdea694bbf15236a26dfa7ebe732359ea095b3a
claudio-walser <walsercl@gmx.ch>
2017-10-28 08:03:23 +0200

    one deeper

--- .travis/clone-test-repo.sh
+++ .travis/clone-test-repo.sh
@@ -1,7 +1,7 @@
 # setup ssh key for https://github.com/claudio-walser/travis-gitcd
 
-git clone git@github.com:claudio-walser/travis-gitcd.git ~/build/travis-gitcd
+git clone git@github.com:claudio-walser/travis-gitcd.git ~/build/claudio-walser/travis-gitcd
 pwd
 ls -hal
-ls -hal ~/build
+ls -hal ~/build/claudio-walser
 which git-cd
