21bde76455a5e91b7e8241a246ef5b4c9522c198
Claudio Walser <claudio.walser@srf.ch>
2017-10-28 07:45:51 +0200

    cloning into home

--- .travis/clone-test-repo.sh
+++ .travis/clone-test-repo.sh
@@ -1,6 +1,6 @@
 # setup ssh key for https://github.com/claudio-walser/travis-gitcd
 
-git clone git@github.com:claudio-walser/travis-gitcd.git
+git clone git@github.com:claudio-walser/travis-gitcd.git ~/travis-gitcd
 pwd
 ls -hal
 ls -hal ~
