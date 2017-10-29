21857692b2d5aa49988bffb41ce7d4d233fe1acd
claudio-walser <walsercl@gmx.ch>
2017-10-30 00:38:32 +0100

    dont get it a the moment

--- .travis/test-git-cd-review.sh
+++ .travis/test-git-cd-review.sh
@@ -2,14 +2,13 @@ set -e
 
 # change workdir to travis-gitcd
 cd ~/build/claudio-walser/travis-gitcd
-echo "before status"
-git status --porcelain
-echo "after status"
 
 /usr/bin/expect <<EOD
 spawn git-cd review
 expect "Your personal Github token?"
 send "$GH_ACCESS_TOKEN\n"
+expect "You currently have uncomitted changes. Do you want me to abort and let you commit first?"
+send "no\n"
 expect "Pull-Request title?"
 send "Pull Request Title for travis build $TRAVIS_JOB_NUMBER\n"
 expect "Pull-Request body?"
