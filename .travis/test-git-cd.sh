04247fdc877190099f225e935d12d49f3fe2d880
claudio-walser <walsercl@gmx.ch>
2017-10-29 07:23:44 +0100

    test-git-cd.sh as well

--- .travis/test-git-cd.sh
+++ .travis/test-git-cd.sh
@@ -1,15 +1,9 @@
-#. .travis/exit-on-error.sh
+set -e
 
 # change workdir to travis-gitcd
 cd ~/build/claudio-walser/travis-gitcd
 
 git-cd start travis-$TRAVIS_JOB_NUMBER
-#exitOnError()
-if [ $? != 0 ]; then
-	echo "Exit code is not 0"
-    exit 1
-fi
-
 
 # change back to original workdir
 cd -
