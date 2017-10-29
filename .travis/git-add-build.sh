3fef3ddf2d57e2657efa10fcca9470db527cceda
claudio-walser <walsercl@gmx.ch>
2017-10-29 18:34:00 +0100

    try review but only in py 3.6 for now

---
+++ .travis/git-add-build.sh
@@ -0,0 +1,11 @@
+set -e
+
+# change workdir to travis-gitcd
+cd ~/build/claudio-walser/travis-gitcd
+
+echo travis-$TRAVIS_JOB_NUMBER >> README.rst
+git commit -m "Add current build number"
+git push
+
+# change back to original workdir
+cd -
---
+++ .travis/test-git-cd-review.sh
@@ -0,0 +1,17 @@
+set -e
+
+# change workdir to travis-gitcd
+cd ~/build/claudio-walser/travis-gitcd
+
+/usr/bin/expect <<EOD
+spawn git-cd review
+expect "Your personal Github token?"
+send "$GH_ACCESS_TOKEN\n"
+expect "Pull-Request title?"
+send "Pull Request Title for travis build $TRAVIS_JOB_NUMBER\n"
+expect "Pull-Request body?"
+send "Pull Request Body for travis build $TRAVIS_JOB_NUMBER\n"
+EOD
+
+# change back to original workdir
+cd -
