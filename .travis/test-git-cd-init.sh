718afb5ab8443aedfcf3c8c64a324682b3239111
claudio-walser <walsercl@gmx.ch>
2017-10-29 09:02:04 +0100

    git-cd init using expect

--- .travis/test-git-cd-init.sh
+++ .travis/test-git-cd-init.sh
@@ -1,3 +1,18 @@
 set -e
 
-which expect
+# git-cd init with accepting all the defaults
+/usr/bin/expect <<EOD
+spawn git-cd init
+expect "Branch name for production releases?"
+send "\n"
+expect "Branch name for feature development?"
+send "\n"
+expect "Branch name for test releases?"
+send "\n"
+expect "Version tag prefix?"
+send "\n"
+expect "Version type? You can either set your tag number manually, read it from a version file or generate it by date."
+send "\n"
+expect "Do you want to execute some additionalcommands after a release?"
+send "\n"
+EOD
