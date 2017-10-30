926e4995a02ecd5ce2317fa3f660f7800cdf7083
Claudio Walser <claudio.walser@srf.ch>
2017-10-30 18:21:41 +0100

    expect is a hard word to write

--- .travis/test-git-cd-release.sh
+++ .travis/test-git-cd-release.sh
@@ -8,7 +8,7 @@ cd ~/build/claudio-walser/travis-gitcd
 spawn git-cd release
 expect "Whats the current tag number you want to deliver?"
 send "$TRAVIS_JOB_NUMBER\n"
-expec "What message your new tag should have?"
+expect "What message your new tag should have?"
 send "New Travis Build $TRAVIS_JOB_NUMBER"
 expect
 EOD
