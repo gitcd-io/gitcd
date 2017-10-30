6f73e71e76ea0404f0d12e206785fe94a4833bb5
Claudio Walser <claudio.walser@srf.ch>
2017-10-30 17:58:12 +0100

    test release process and fix a new bug with extraCommand

---
+++ .travis/test-git-cd-release.sh
@@ -0,0 +1,17 @@
+set -e
+
+# change workdir to travis-gitcd
+cd ~/build/claudio-walser/travis-gitcd
+
+# git-cd finish with deleting the feature branch
+/usr/bin/expect <<EOD
+spawn git-cd release
+expect "Whats the current tag number you want to deliver?"
+send "$TRAVIS_JOB_NUMBER\n"
+expec "What message your new tag should have?"
+send "New Travis Build $TRAVIS_JOB_NUMBER"
+expect
+EOD
+
+# change back to original workdir
+cd -
