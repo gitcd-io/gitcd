015018a8a2d92cd225ee601f7d9b8a2983920df0
claudio-walser <walsercl@gmx.ch>
2017-10-30 20:02:41 +0100

    add job number to commit message and some debug

--- .travis/test-git-cd-start.sh
+++ .travis/test-git-cd-start.sh
@@ -6,6 +6,8 @@ cd ~/build/claudio-walser/travis-gitcd
 git-cd start travis-$TRAVIS_JOB_NUMBER
 
 # assert that new feature branch exists remote
+git branch -a | grep "origin/feature/travis-$TRAVIS_JOB_NUMBER"
+echo $?
 
 # change back to original workdir
 cd -
