7e6092da2334736ba16a608f027ff490d186b3cf
Claudio Walser <claudio.walser@srf.ch>
2017-10-31 07:54:33 +0100

    git-cd clean test should work

--- .travis/test-git-cd-clean.sh
+++ .travis/test-git-cd-clean.sh
@@ -3,13 +3,32 @@ set -e
 # change workdir to travis-gitcd
 cd ~/build/claudio-walser/travis-gitcd
 
+# start a feature
 git-cd start travis-clean-$TRAVIS_JOB_NUMBER
 
 # assert that new feature branch exists remote
 git branch -a | grep "origin/feature/travis-clean-$TRAVIS_JOB_NUMBER"
 
+# delete feature branch on remote
 git push origin :feature/travis-clean-$TRAVIS_JOB_NUMBER
 
+# assert that the feature branch not present on remote anymore
+REMOTE_COUNT=`git branch -a | grep "origin/feature/travis-clean-$TRAVIS_JOB_NUMBER" | wc -l`
+if [ $REMOTE_COUNT != 0 ]; then
+	echo "Feature Branch still found on remote"
+	exit 1
+fi
+
+# call git-cd clean
 git-cd clean
+
+# assert the local feature branch is deleted
+# assert that the feature branch not present on remote anymore
+LOCAL_COUNT=`git branch -a | grep "feature/travis-clean-$TRAVIS_JOB_NUMBER" | wc -l`
+if [ $LOCAL_COUNT != 0 ]; then
+	echo "Feature Branch still found locally"
+	exit 1
+fi
+
 # change back to original workdir
 cd -
