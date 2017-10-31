0c21855ed153b7487dcef1e563fd59945298067a
Claudio Walser <claudio.walser@srf.ch>
2017-10-31 08:06:12 +0100

    added tests for git-cd test and execute clean afterwards

---
+++ .travis/test-git-cd-test.sh
@@ -0,0 +1,21 @@
+set -e
+
+# change workdir to travis-gitcd
+cd ~/build/claudio-walser/travis-gitcd
+
+# start a feature
+git-cd start travis-test-$TRAVIS_JOB_NUMBER
+
+# assert that new feature branch exists remote
+git branch -a | grep "origin/feature/travis-test-$TRAVIS_JOB_NUMBER"
+
+
+# call git-cd clean
+git-cd test
+
+# assert the feature branch is checked out again
+# assert there is no diff against the test branch
+
+
+# change back to original workdir
+cd -
