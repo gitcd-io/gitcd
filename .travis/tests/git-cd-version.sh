5159a6c232bf07fb139e1b77f330bf59354ba712
Claudio Walser <claudio.walser@srf.ch>
2019-03-14 07:45:30 +0100

    Get some debug output

--- .travis/tests/git-cd-version.sh
+++ .travis/tests/git-cd-version.sh
@@ -1,6 +1,8 @@
 set -e
 
 CURRENT_VERSION=$(cat version.txt)
+echo "Current version var is: $CURRENT_VERSION\n"
+echo "You run git-cd in version $CURRENT_VERSION\n"
 
 /usr/bin/expect <<EOD
 spawn git-cd version
