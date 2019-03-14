cafba652b5e1184c255a31ec5aab2c7f1796e860
Claudio Walser <claudio.walser@srf.ch>
2019-03-14 07:48:23 +0100

    No need to use expect since I dont have to send user input back

--- .travis/tests/git-cd-version.sh
+++ .travis/tests/git-cd-version.sh
@@ -1,11 +1,4 @@
 set -e
 
 CURRENT_VERSION=$(cat version.txt)
-echo "Current version var is: $CURRENT_VERSION\n"
-echo "You run git-cd in version $CURRENT_VERSION\n"
-
-/usr/bin/expect <<EOD
-spawn git-cd version
-expect "You run git-cd in version $CURRENT_VERSION\n"
-expect
-EOD
+git-cd version | grep "You run git-cd in version $CURRENT_VERSION"
