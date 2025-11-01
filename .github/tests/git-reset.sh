87191ca12b54eec055622c00ec6e705ec16db465
Claudio <info@gitcd.io>
2025-11-01 07:48:34 +0100

    Add cleanup step to remove leftover test branches from previous runs

--- .github/tests/git-reset.sh
+++ .github/tests/git-reset.sh
@@ -3,7 +3,21 @@ set -e
 # change workdir to travis-gitcd
 cd ~/build/gitcd-io/travis-gitcd
 
+# Save current branch name
+CURRENT_BRANCH=$(git branch --show-current)
+
+# Reset any local changes
 git reset --hard
+
+# Checkout master to allow cleanup
+git checkout master
+
+# Delete local feature branches that might exist
+git branch -D "feature/github-$GITHUB_RUN_NUMBER" 2>/dev/null || true
+git branch -D "test/github-$GITHUB_RUN_NUMBER" 2>/dev/null || true
+
+# Reset master to origin
+git reset --hard origin/master
 
 # change back to original workdir
 cd -
